a8c94c222760356b540dd3f9bb059c7fae12204e
Koersken <48862837+Koersken@users.noreply.github.com>
2023-10-21 01:51:26 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,55 @@
+from dotenv import load_dotenv
 import streamlit as st
-from gpt4all import GPT4ALL
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
 
-model = GPT4ALL("GPT4All-13B-snoozy.ggmlv3.q4_0.bin")
 
-# Storage
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-# Display history
-for message in st.session_state.messages:
-    with st.chat_message(message.get("role")):
-        st.write(message.get("content"))
-
-prompt = st.chat_input("Ask something about TIP!")
-
-if prompt:
-    # Add to storage
-    st.session_state.messages.append({"role": "user","content":prompt})
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Ask your PDF")
+    st.header("Ask your PDF 💬")
     
-    # Display message
-    with st.chat_message("user"):
-        st.write(prompt)
+    # upload file
+    pdf = st.file_uploader("Upload your PDF", type="pdf")
     
-    # Process response
-    result = model.chat_completion({"role": "assistant","content":prompt})
-    response = result["choices"][0]["message"]["content"]
+    # extract the text
+    if pdf is not None:
+      pdf_reader = PdfReader(pdf)
+      text = ""
+      for page in pdf_reader.pages:
+        text += page.extract_text()
+        
+      # split into chunks
+      text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+      )
+      chunks = text_splitter.split_text(text)
+      
+      # create embeddings
+      embeddings = OpenAIEmbeddings()
+      knowledge_base = FAISS.from_texts(chunks, embeddings)
+      
+      # show user input
+      user_question = st.text_input("Ask a question about your PDF:")
+      if user_question:
+        docs = knowledge_base.similarity_search(user_question)
+        
+        llm = OpenAI()
+        chain = load_qa_chain(llm, chain_type="stuff")
+        with get_openai_callback() as cb:
+          response = chain.run(input_documents=docs, question=user_question)
+          print(cb)
+           
+        st.write(response)
     
-    # Store response
-    st.session_state.messages.append({"role": "assistant","content":response})
 
-    # Display response
-    with st.chat_message("assistant"):
-        st.markdown(response)
+if __name__ == '__main__':
+    main()
